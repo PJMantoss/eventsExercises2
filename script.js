d769ac8bdd351999dbc3a7470ed6ddc9226c9d4a
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2020-02-24 15:10:10 +0100

    update script

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
-    
+    let todoForm = document.getElementById("newTodoForm");
+    let todoList = document.getElementById("todoList")
+
+    todoForm.addEventListener("submit", function(event){
+        event.preventDefault();
+    })
 })
